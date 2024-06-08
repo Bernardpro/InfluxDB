d7dd33dfa7971e198273f45467aa8bf28cb4bab8
Bernardpro <107325803+Bernardpro@users.noreply.github.com>
2024-06-08 19:33:52 +0200

    Add doc to function

--- transfer_influx.py
+++ transfer_influx.py
@@ -6,10 +6,20 @@ from sqlalchemy import create_engine
 from sqlalchemy.sql import text
 from dotenv import load_dotenv
 import os
+import sqlalchemy
 
 load_dotenv()
 
-def init_connection(sql_connexion):
+def init_connection(sql_connexion: str):
+    """
+    Initializes a connection to the SQL Server database.
+
+    Parameters:
+    sql_connexion (str): The connection string for SQL Server.
+
+    Returns:
+    engine: SQLAlchemy engine object for the SQL Server connection.
+    """
     connection_string = rf"{sql_connexion}"
     connection_url = URL.create("mssql+pyodbc", query={"odbc_connect": connection_string})
     engine = create_engine(connection_url, fast_executemany=True)
@@ -29,7 +39,13 @@ print(bucket, org, token, url)
 client = InfluxDBClient(url=url, token=token, org=org)
 
 # Function to write data points to InfluxDB
-def write_temperature_data(row: list):
+def write_temperature_data(row: sqlalchemy.engine.cursor.CursorResult):
+    """
+    Writes a row of data to InfluxDB.
+
+    Parameters:
+    row (sqlalchemy.engine.cursor.CursorResult): A 'tuple like' containing the row data from SQL Server.
+    """
     # Create a data point
     for index, col in enumerate(columns[1:], 1):
         point = Point("_historique") \
